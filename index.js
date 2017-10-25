2c701d1f65732688c30d585ffdd458b0971099a8
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-10-25 18:14:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function happyHolidays(){
   return "Happy holidays!";
 }
 
-function happyHolidaysTo(x){
-  return "Happy holidays, ${x}!";
+function happyHolidaysTo(name){
+  return "Happy holidays, " + name + "!";
 }
 //
 // function happyHolidayTo(holiday,name){
